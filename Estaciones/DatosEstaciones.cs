4eac369d64ec90393d14550df3331f701c656572
Triste9 <oscar.99.tris@gmail.com>
2017-10-13 18:33:56 -0500

    Agregar SQL server

---
+++ Estaciones/DatosEstaciones.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Estaciones
+{
+    class DatosEstaciones
+    {
+        public String conexion = "Data Source=192.168.100.20;Initial Catalog=5Estaciones;Persist Security Info=True;User ID=5Estaciones;Password=12345";
+        public DatosEstaciones()
+        {
+            
+            SqlConnection cnn = new SqlConnection(conexion);
+            try
+            {
+                cnn.Open();
+                MessageBox.Show("Se abrió la conexión con el servidor SQL Server y se seleccionó la base de datos");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Esto no vale verga compa");
+            }   
+            cnn.Close();
+            MessageBox.Show("Se cerró la conexión.");
+        }
+        public void InsertTableOne(string qr, double valor, int ok)
+        {
+            using (SqlConnection connection = new SqlConnection(this.conexion))
+            {
+                using (SqlCommand command = new SqlCommand())
+                {
+                    command.Connection = connection;            // <== lacking
+                    command.CommandType = CommandType.Text;
+                    command.CommandText = "INSERT INTO Estacion1(QRest1,Medicion1,Fechareg1,OK1) VALUES (@qr, @valor,SYSDATETIME() , @ng_ok)";
+                    command.Parameters.AddWithValue("@qr", qr);
+                    command.Parameters.AddWithValue("@valor", valor);
+                    command.Parameters.AddWithValue("@ng_ok", ok);
+                    try
+                    {
+                        connection.Open();
+                        int recordsAffected = command.ExecuteNonQuery();
+                    }
+                    catch (SqlException)
+                    {
+                        MessageBox.Show("No se puedo hacer este desmadre.");
+                    }
+                    finally
+                    {
+                        connection.Close();
+                    }
+                }
+            }
+        }
+
+        public void InsertEstacion2(string qr, double valor,double valor2, double valor3, int ok)
+        {
+            using (SqlConnection connection = new SqlConnection(this.conexion))
+            {
+                using (SqlCommand command = new SqlCommand())
+                {
+                    command.Connection = connection;            // <== lacking
+                    command.CommandType = CommandType.Text;
+                    command.CommandText = "INSERT INTO Estacion2(QRest1,Medicion2,Medicion2_1,Medicion2_2,Fechareg1,OK1) VALUES (@qr, @valor,@valor1,@valor2,SYSDATETIME() , @ng_ok)";
+                    command.Parameters.AddWithValue("@qr", qr);
+                    command.Parameters.AddWithValue("@valor", valor);
+                    command.Parameters.AddWithValue("@valor1", valor2);
+                    command.Parameters.AddWithValue("@valor2", valor3);
+                    command.Parameters.AddWithValue("@ng_ok", ok);
+                    try
+                    {
+                        connection.Open();
+                        int recordsAffected = command.ExecuteNonQuery();
+                    }
+                    catch (SqlException)
+                    {
+                        MessageBox.Show("No se puedo hacer este desmadre.");
+                    }
+                    finally
+                    {
+                        connection.Close();
+                    }
+                }
+            }
+        }
+    }
+
+}
--- Estaciones/MainWindow.xaml.cs
+++ Estaciones/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
 
 namespace Estaciones
 {
@@ -20,99 +22,233 @@ namespace Estaciones
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
             qr.Text = "6879363";
-            int dqr = 123456;
-
+            DatosEstaciones pruebas = new DatosEstaciones();
+            
 
         }
+        public double v1st1_;
+        public double v1st2_;
+        public double v2st2_;
+        public double v3st2_;
+        public double v1st3_;
+        public double v2st3_;
+        public double v3st3_;
+        public double v4st3_;
+        public double v1st4_;
+        public double v2st4_;
+        public double v3st4_;
 
         private void MainGrid_Loaded(object sender, RoutedEventArgs e)
         {
-            AgregarDatosTabla();
+            ThreadStart delegado = new ThreadStart(CorrerProceso);
+           
+            Thread hilo = new Thread(delegado);
+ 
+            hilo.Start();
+            ThreadStart delegado1= new ThreadStart(CorresProceso2);
 
+            Thread hilo1 = new Thread(delegado1);
+
+            hilo1.Start();
 
         }
+        
 
-        private void AgregarDatosTabla()
+        private void CambiarDatos()
         {
-            var data = new Test
+            Random random = new Random();
+            this.v1st1_ = random.NextDouble(45.23, 45.80);
+            this.v1st2_ = random.NextDouble(40.5, 40.539);
+            this.v2st2_ = random.NextDouble(124.7, 124.780);
+            this.v1st3_ = random.NextDouble(30.4, 30.8);
+            this.v2st3_ = random.NextDouble(27.6, 27.9);
+            this.v3st3_ = random.NextDouble(44.5, 40.69);
+            this.v4st3_ = random.NextDouble(44.5, 40.7);
+            this.v1st4_ = random.NextDouble(36.4, 36.6);
+            this.v2st4_ = random.NextDouble(38.1, 38.2);
+            this.v3st4_ = random.NextDouble(135.2, 135.6);
+        }
+        private void CorrerProceso()
+        {
+            while (true)
             {
-                date = "14/08/2017 13:42:49",
-                code = "170814A0001",
+                CambiarDatos();
+                Thread.Sleep(1000);
+            }
+        }
 
-                valorst1 = 45.774,
-                okst1 = 1,
-                ngst1 = 0,
+        private void CorresProceso2()
+        {
+            while (true)
+            {
+                AgregarDatosTabla();
+                Thread.Sleep(3000);
+            }
+        }
 
-                valorst2 = 40.540,
-                okst2 = 1,
-                ngst2 = 0,
+        
 
-                valorst3 = 64.58,
-                okst3 = 0,
-                ngst3 = 1,
+    private void AgregarDatosTabla()
+        {
+            Random r = new Random();
+            int aleatorio = r.Next(0, 1000);
 
-                valorst4 = 67.6,
-                okst4 = 0,
-                ngst4 = 1,
+            
+            Application.Current.Dispatcher.Invoke(new Action(() => {
+                this.qr.Text = Convert.ToString(aleatorio);
+            }));
+            int okst1;
+            int okst2;
+            int okst3;
+            int okst4;
 
-                gauNg = 0,
-                gauOk = 1,
-                visNg = 0,
-                visOk = 1
+            int ngst1;
+            int ngst2;
+            int ngst3;
+            int ngst4;
 
-            };
+            int gauNg;
+            int gauOk;
 
-            if(data.valorst1 > 45.70+0.075 || data.valorst1 < 45.70 - 0.075)
+            
+
+            if(this.v1st1_ > 45.70+0.075 ||this.v1st1_ < 45.70 - 0.075)
             {
                 CambiarEstadoBoton(1,1);
+                okst1 = 0;
+                ngst1 = 1;
             }
             else
             {
+                ngst1 = 0;
+                okst1 = 1;
                 CambiarEstadoBoton(1, 0);
             }
 
-            if(data.valorst2 >= 40.5 && data.valorst2 <= 40.5+0.039)
+            if(this.v1st2_ >= 40.5 && this.v1st2_ <= 40.5+0.039)
             {
                 CambiarEstadoBoton(2, 0);
-            }else
+                okst2= 1;
+                ngst2 = 0;
+                
+            }
+            else
             {
+                
                 CambiarEstadoBoton(2, 1);
+                ngst2 = 1;
+                okst2 = 0;
+
             }
 
-            if(data.valorst3 > 30.5 + 0.2 || data.valorst3 < 30.5 - 0.2)
+            if( this.v1st3_ > 30.5 + 0.2 || this.v1st3_ < 30.5 - 0.2)
             {
                 CambiarEstadoBoton(3, 1);
+                okst3 = 0;
+                ngst3 = 1; 
+                
+                
             }
             else
             {
                 CambiarEstadoBoton(3, 0);
+                ngst3 = 0;
+                okst3 = 1;
 
             }
 
-            if(data.valorst4 > 36.4 +  0.15 || data.valorst4< 36.4)
+            if( this.v1st4_ > 36.4 +  0.15 || this.v1st4_ < 36.4)
             {
                 CambiarEstadoBoton(4, 1);
+                okst4 = 0;
+                ngst4 = 1;
 
             }
             else
             {
                 CambiarEstadoBoton(4, 0);
+                ngst4 = 0;
+                okst4 = 1;
 
             }
 
+            if (okst1 == 1 && okst2 == 1 && okst3 == 1 && okst4 == 1)
+            {
+                gauOk = 1;
+                gauNg = 0;
+
+                Application.Current.Dispatcher.Invoke(new Action(() => {
+                    int num = Convert.ToInt32(this.ok.Text);
+                    num = num + 1;
+                    this.ok.Text = Convert.ToString(num);
+                }));
+
+                
+            }else
+            {
+                gauOk = 0;
+                gauNg = 1;
+
+                Application.Current.Dispatcher.Invoke(new Action(() => {
+                    int num = Convert.ToInt32(this.ng.Text);
+                    num = num + 1;
+                    this.ng.Text = Convert.ToString(num);
+                }));
+
+                
+            }
+
+            var data = new Test
+            {
+                date = "14/08/2017 13:42:49",
+                code = "170814A0001",
+
+                valorst1 = this.v1st1_,
+                okst1 = okst1,
+                ngst1 = ngst1,
+
+                valorst2 = this.v1st2_,
+                okst2 = okst2,
+                ngst2 = ngst2,
+
+                valorst3 = this.v1st3_,
+                okst3 = okst3,
+                ngst3 = ngst3,
+
+                valorst4 = this.v1st4_,
+                okst4 = okst4,
+                ngst4 = ngst4,
+
+                gauNg = gauNg,
+                gauOk = gauOk,
+                visNg = 0,
+                visOk = 1
+
+            };
 
 
+            Application.Current.Dispatcher.Invoke(new Action(() => {
+                DatosGrid.Items.Add(data);
+                this.v1st1.Text = Convert.ToString(v1st1_);
+                this.v1st2.Text = Convert.ToString(v1st2_);
+                this.v2st2.Text = Convert.ToString(v2st2_);
+                this.v1st3.Text = Convert.ToString(v1st3_);
+                this.v2st3.Text = Convert.ToString(v2st3_);
+                this.v3st3.Text = Convert.ToString(v3st3_);
+                this.v4st3.Text = Convert.ToString(v4st3_);
+                this.v1st4.Text = Convert.ToString(v1st4_);
+                this.v2st4.Text = Convert.ToString(v2st4_);
+                this.v3st4.Text = Convert.ToString(v3st4_);
 
-            DatosGrid.Items.Add(data);
-            DatosGrid.Items.Add(data);
-            DatosGrid.Items.Add(data);
-            DatosGrid.Items.Add(data);
-            DatosGrid.Items.Add(data);
-            //DatosGrid.Items.Clear();
+            }));
+
+           
+            
         }
 
         private void CambiarEstadoBoton(int stNumero, int verdeRojo)
@@ -128,32 +264,88 @@ namespace Estaciones
                 path = @"C:\Users\Oscar Martinez\Documents\Visual Studio 2015\Projects\Estaciones\Estaciones\boton.png";
 
             }
+            Application.Current.Dispatcher.Invoke(new Action(() => {
 
-            switch (stNumero)
+                switch (stNumero)
+                {
+                    case 1:
+                        st1.Fill = new ImageBrush(new BitmapImage(
+       new Uri(path, UriKind.Relative)));
+                        break;
+                    case 2:
+                        st2.Fill = new ImageBrush(new BitmapImage(
+       new Uri(path, UriKind.Relative)));
+                        break;
+                    case 3:
+                        st3.Fill = new ImageBrush(new BitmapImage(
+       new Uri(path, UriKind.Relative)));
+                        break;
+                    case 4:
+                        st4.Fill = new ImageBrush(new BitmapImage(
+       new Uri(path, UriKind.Relative)));
+                        break;
+                    case 5:
+                        gaCheck.Fill = new ImageBrush(new BitmapImage(
+       new Uri(path, UriKind.Relative)));
+                        break;
+                    default:
+                        break;
+                }
+
+            }));
+
+        }
+
+        private void Cuando_haga_Click(object sender, RoutedEventArgs e)
+        {
+
+            MessageBox.Show("Hola pinche putita");
+
+        }
+
+        private void Btn1(object sender, RoutedEventArgs e)
+        {
+            string qrTb1 = "98273498742";
+            Random random = new Random();
+            this.v1st1_ = random.NextDouble(45.23, 45.80);
+            int okst1;
+
+            if (this.v1st1_ > 45.70 + 0.075 || this.v1st1_ < 45.70 - 0.075)
             {
-                case 1:
-                    st1.Fill = new ImageBrush(new BitmapImage(
-   new Uri(path, UriKind.Relative)));
-                    break;
-                case 2:
-                    st2.Fill = new ImageBrush(new BitmapImage(
-   new Uri(path, UriKind.Relative)));
-                    break;
-                case 3:
-                    st3.Fill = new ImageBrush(new BitmapImage(
-   new Uri(path, UriKind.Relative)));
-                    break;
-                case 4:
-                    st4.Fill = new ImageBrush(new BitmapImage(
-   new Uri(path, UriKind.Relative)));
-                    break;
-                case 5:
-                    gaCheck.Fill = new ImageBrush(new BitmapImage(
-   new Uri(path, UriKind.Relative)));
-                    break;
-                default:
-                    break;
+                CambiarEstadoBoton(1, 1);
+                okst1 = 0;
+               
+            }else
+            {
+                okst1 = 1;
             }
+            DatosEstaciones ComoYoQuieraWUeyNohayPedo = new DatosEstaciones();
+            ComoYoQuieraWUeyNohayPedo.InsertTableOne(qrTb1, this.v1st1_,okst1);
+
+        }
+
+        private void Btn2(object sender, RoutedEventArgs e)
+        {
+            string qrTb1 = "98273498742";
+            Random random = new Random();
+            this.v1st2_ = random.NextDouble(45.23, 45.80);
+            this.v2st2_ = random.NextDouble(45.23, 45.80);
+            this.v3st2_ = random.NextDouble(45.23, 45.80);
+            int okst1;
+            if (((this.v1st2_ >= 124.7 && this.v1st2_ <= 124.7 + 0.075) && (this.v1st2_ >= 124.7 && this.v1st2_ <= 124.7)) && (this.v2st2_ >= 124.7 && this.v2st2_ <= 124.7 + 0.075))
+            {
+                CambiarEstadoBoton(1, 1);
+                okst1 = 0;
+            }
+            else
+            {
+                okst1 = 1;
+            }
+            DatosEstaciones ComoYoQuieraWUeyNohayPedo = new DatosEstaciones();
+            ComoYoQuieraWUeyNohayPedo.InsertEstacion2(qrTb1, this.v1st2_, this.v2st2_, this.v3st2_, okst1);
+
+
+
 
         }
     }
@@ -181,5 +373,15 @@ namespace Estaciones
         public int gauNg { get; set; }
         public int visOk { get; set; }
         public int visNg { get; set; }
+    }
+    public static class RandomExtensions
+    {
+        public static double NextDouble(
+            this Random random,
+            double minValue,
+            double maxValue)
+        {
+            return random.NextDouble() * (maxValue - minValue) + minValue;
+        }
     }
 }
