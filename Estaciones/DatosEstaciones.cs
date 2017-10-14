4a01a3c6c3e524166ad65bfcce89d5f2b478f3b0
lezpaul26 <francisco.salas@icma-ingenieria.com>
2017-10-14 07:33:13 -0500

    consultas sql
    cambios en las los sql dependiendo de la estacion

--- Estaciones/DatosEstaciones.cs
+++ Estaciones/DatosEstaciones.cs
@@ -11,6 +11,7 @@ namespace Estaciones
 {
     class DatosEstaciones
     {
+        int contador;
         public String conexion = "Data Source=192.168.100.20;Initial Catalog=5Estaciones;Persist Security Info=True;User ID=5Estaciones;Password=12345";
         public DatosEstaciones()
         {
@@ -57,7 +58,7 @@ namespace Estaciones
             }
         }
 
-        public void InsertEstacion2(string qr, double valor,double valor2, double valor3, int ok)
+        public void InsertEstacion1(string qr, double valor, int ok)
         {
             using (SqlConnection connection = new SqlConnection(this.conexion))
             {
@@ -65,7 +66,37 @@ namespace Estaciones
                 {
                     command.Connection = connection;            // <== lacking
                     command.CommandType = CommandType.Text;
-                    command.CommandText = "INSERT INTO Estacion2(QRest1,Medicion2,Medicion2_1,Medicion2_2,Fechareg1,OK1) VALUES (@qr, @valor,@valor1,@valor2,SYSDATETIME() , @ng_ok)";
+                    command.CommandText = "INSERT INTO Estacion1(QRest1,Medicion2,Fechareg1,OK1) VALUES (@qr, @valor,SYSDATETIME() , @ng_ok)";
+                    command.Parameters.AddWithValue("@qr", qr);
+                    command.Parameters.AddWithValue("@valor", valor);
+                    command.Parameters.AddWithValue("@ng_ok", ok);
+                    try
+                    {
+                        connection.Open();
+                        int recordsAffected = command.ExecuteNonQuery();
+                    }
+                    catch (SqlException)
+                    {
+                        MessageBox.Show("No se puedo hacer este desmadre.");
+                    }
+                    finally
+                    {
+                        connection.Close();
+                    }
+                }
+            }
+        }
+    }
+
+    public void InsertEstacion2(string qr, double valor,double valor2, double valor3, int ok)
+        {
+            using (SqlConnection connection = new SqlConnection(this.conexion))
+            {
+                using (SqlCommand command = new SqlCommand())
+                {
+                    command.Connection = connection;            // <== lacking
+                    command.CommandType = CommandType.Text;
+                    command.CommandText = "INSERT INTO Estacion2(QRest2,Medicion2,Medicion2_1,Medicion2_2,Fechareg1,OK1) VALUES (@qr, @valor,@valor1,@valor2,SYSDATETIME() , @ng_ok)";
                     command.Parameters.AddWithValue("@qr", qr);
                     command.Parameters.AddWithValue("@valor", valor);
                     command.Parameters.AddWithValue("@valor1", valor2);
@@ -87,6 +118,133 @@ namespace Estaciones
                 }
             }
         }
-    }
 
+
+    public void InsertEstacion3(string qr, double valor, int ok)
+        {
+            using (SqlConnection connection = new SqlConnection(this.conexion))
+            {
+                using (SqlCommand command = new SqlCommand())
+                {
+                    command.Connection = connection;            // <== lacking
+                    command.CommandType = CommandType.Text;
+                    command.CommandText = "INSERT INTO Estacion3(QRest3,Medicion3,Fechareg1,OK1) VALUES (@qr, @valor,SYSDATETIME() , @ng_ok)";
+                    command.Parameters.AddWithValue("@qr", qr);
+                    command.Parameters.AddWithValue("@valor", valor);
+                    command.Parameters.AddWithValue("@ng_ok", ok);
+                    try
+                    {
+                        connection.Open();
+                        int recordsAffected = command.ExecuteNonQuery();
+                    }
+                    catch (SqlException)
+                    {
+                        MessageBox.Show("No se puedo hacer este desmadre.");
+                    }
+                    finally
+                    {
+                        connection.Close();
+                    }
+                }
+            }
+        }
+
+public void InsertEstacion4(string qr, double valor, int ok)
+        {
+            using (SqlConnection connection = new SqlConnection(this.conexion))
+            {
+                using (SqlCommand command = new SqlCommand())
+                {
+                    command.Connection = connection;            // <== lacking
+                    command.CommandType = CommandType.Text;
+                    command.CommandText = "INSERT INTO Estacion4(QRest4,Medicion4,Fechareg1,OK1) VALUES (@qr, @valor,SYSDATETIME() , @ng_ok)";
+                    command.Parameters.AddWithValue("@qr", qr);
+                    command.Parameters.AddWithValue("@valor", valor);
+                    command.Parameters.AddWithValue("@ng_ok", ok);
+                    try
+                    {
+                        connection.Open();
+                        int recordsAffected = command.ExecuteNonQuery();
+                        cada30();
+                    }
+                    catch (SqlException)
+                    {
+                        MessageBox.Show("No se puedo hacer este desmadre.");
+                    }
+                    finally
+                    {
+                        connection.Close();
+                    }
+                }
+            }
+        }
+
+public void InsertEstacion4_1(string qr, double valor, int ok)
+        {
+            using (SqlConnection connection = new SqlConnection(this.conexion))
+            {
+                using (SqlCommand command = new SqlCommand())
+                {
+                    command.Connection = connection;            // <== lacking
+                    command.CommandType = CommandType.Text;
+                    command.CommandText = "INSERT INTO Estacion2(QRest1,Medicion2,Fechareg1,OK1) VALUES (@qr, @valor,SYSDATETIME() , @ng_ok)";
+                    command.Parameters.AddWithValue("@qr", qr);
+                    command.Parameters.AddWithValue("@valor", valor);
+                    command.Parameters.AddWithValue("@ng_ok", ok);
+                    try
+                    {
+                        connection.Open();
+                        int recordsAffected = command.ExecuteNonQuery();
+                    }
+                    catch (SqlException)
+                    {
+                        MessageBox.Show("No se puedo hacer este desmadre.");
+                    }
+                    finally
+                    {
+                        connection.Close();
+                    }
+                }
+            }
+        }
+
+public void InsertEstacion5(string qr, int GAUGE_CHECK, int VISUALCHECK, int ok)
+        {
+            using (SqlConnection connection = new SqlConnection(this.conexion))
+            {
+                using (SqlCommand command = new SqlCommand())
+                {
+                    command.Connection = connection;            // <== lacking
+                    command.CommandType = CommandType.Text;
+                    command.CommandText = "INSERT INTO Estacion5(QRest1,GAUGE_CHECK,VISUALCHECK,Fechareg1,OK1) VALUES (@qr,@GAUGE_CHECK,@VISUALCHECK,SYSDATETIME(), @ng_ok)";
+                    command.Parameters.AddWithValue("@qr", qr);
+                    command.Parameters.AddWithValue("@GAUGE_CHECK", GAUGE_CHECK);
+                    command.Parameters.AddWithValue("@VISUALCHECK", VISUALCHECK); 
+                    command.Parameters.AddWithValue("@ng_ok", ok);
+                    try
+                    {
+                        connection.Open();
+                        int recordsAffected = command.ExecuteNonQuery();
+                    }
+                    catch (SqlException)
+                    {
+                        MessageBox.Show("No se puedo hacer este desmadre.");
+                    }
+                    finally
+                    {
+                        connection.Close();
+                    }
+                }
+            }
+        }
+
+private void cada30(){
+    if (contador <30){
+        contador+1;
+    }
+    else{
+        contador=0;
+        InsertEstacion4_1(string qr, double valor, int ok);
+    }
+}
 }
